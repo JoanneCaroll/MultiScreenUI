f11e5ef87bcedfdc986544fe42a4ea07e21ef0eb
Joanne Caroll <joannecaroll@UNIT001.(none)>
2014-10-28 13:49:22 +0800

    App can be viewed in dual pane in large and in sw600dp devices

---
+++ MultiScreenUI/src/com/example/multiscreenui/MultiScreen2Fragment.java
@@ -0,0 +1,5 @@
+package com.example.multiscreenui;
+
+public class MultiScreen2Fragment {
+
+}
--- MultiScreenUI/src/com/example/multiscreenui/MultiScreenActivity.java
+++ MultiScreenUI/src/com/example/multiscreenui/MultiScreenActivity.java
@@ -2,14 +2,21 @@ package com.example.multiscreenui;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
 
 
 public class MultiScreenActivity extends FragmentActivity {
 
+	 boolean mIsDualPane;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_multi_screen);
+        setContentView(R.layout.activity_multi_screen);  
+        View screenView = findViewById(R.id.headlines2);
+        mIsDualPane = screenView != null && 
+        		screenView.getVisibility() == View.VISIBLE;
+
     }
 
 }
